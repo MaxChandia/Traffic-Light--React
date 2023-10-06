6598055d7b6828e330b80f0583eba1cf7ad3fb67
Max Chandía <Max@MacBook-Pro-de-Max.local>
2023-10-06 12:49:08 -0300

    third commit

--- src/LightTraffic.jsx
+++ src/LightTraffic.jsx
@@ -3,51 +3,62 @@ import { useState } from "react";
 
 
 function LightTraffic () {
-    const [color1, setColor1] = useState("#A42509");
+    const [colorRed, setColorRed] = useState("#A42509");
 
-  const toggleColor = () => {
-    setColor1(prevColor => prevColor === "#A42509" ? 'red' : '#A42509');
+  const toggleColorRed = () => {
+    setColorRed(prevColor => prevColor === "#A42509" ? 'red' : '#A42509');
   };
 
-  const [color2, setColor2] = useState("#CBD11C");
+  const [colorYellow, setColorYellow] = useState("#CBD11C");
 
-  const toggleColor2 = () => {
-    setColor2(prevColor => prevColor === "#CBD11C " ? '#F3FA23' : '#CBD11C ');
+  const toggleColorYellow = () => {
+    setColorYellow(prevColor => prevColor === "#CBD11C " ? '#F3FA23' : '#CBD11C ');
   };
 
-  const [color3, setColor3] = useState("green");
+  const [colorGreen, setColorGreen] = useState("#1B6921");
 
-  const toggleColor3 = () => {
-    setColor3(prevColor => prevColor === "green" ? 'green' : 'green');
+  const toggleColorGreen = () => {
+    setColorGreen(prevColor => prevColor === "#1B6921" ? '#30D53D' : '#1B6921');
   };
 
     return (
     <div className="container">
+            <div className="topSemaforo" style={{width: "15px",
+        height: "50px",
+        background: "black",
+        marginLeft: "auto",
+        marginRight: "auto",
+        marginBottom: "0px"
+        }}></div>
          <div className="semaforo" style={{
             width: "80px",
             height: "170px",
             background: "black",
-            borderRadius: "10%"
+            borderRadius: "10%",
+            marginLeft: "auto",
+            marginRight: "auto",
+            marginTop: "0px"
+            
         }}>
-            <div onClick= {toggleColor} className="red" style={{
+            <div onClick= {toggleColorRed} className="redLight" style={{
                 width: "50px",
                 height: "50px",
                 borderRadius: "50%",
-                background: color1,
+                background: colorRed,
+                margin: "0px 15px",
+            }}></div>
+            <div onClick= {toggleColorYellow} className="yellowLight" style={{
+                width: "50px",
+                height: "50px",
+                borderRadius: "50%",
+                background: colorYellow,
                 margin: "5px 15px"
             }}></div>
-            <div onClick= {toggleColor2} className="red" style={{
+            <div onClick= {toggleColorGreen} className="greenLight" style={{
                 width: "50px",
                 height: "50px",
                 borderRadius: "50%",
-                background: color2,
-                margin: "5px 15px"
-            }}></div>
-            <div onClick= {toggleColor3} className="red" style={{
-                width: "50px",
-                height: "50px",
-                borderRadius: "50%",
-                background: color3,
+                background: colorGreen,
                 margin: "5px 15px",
             }}></div>
             
